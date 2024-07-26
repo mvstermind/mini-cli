88772b728297068022ae6cd6930d2a856b2df7bc
mvstermind <zablockidawid1@gmail.com>
2024-07-26 22:18:54 +0200

    added more comments for docs

--- minicli.go
+++ minicli.go
@@ -1,10 +1,6 @@
 package minicli
 
-import (
-	"fmt"
-	"os"
-)
-
+// Arg struct containts all information about command line argument
 type Arg struct {
 	ShortCmd string
 	LongCmd  string
@@ -12,8 +8,11 @@ type Arg struct {
 	Required bool
 }
 
+// type Commands keeps all of the command line arguments
+// in slice of *Arg
 type Commands []*Arg
 
+// NewArg creates new argument for cli
 func NewArg(short, long, desc string, required bool) *Arg {
 	return &Arg{
 		ShortCmd: short,
@@ -23,6 +22,9 @@ func NewArg(short, long, desc string, required bool) *Arg {
 	}
 }
 
+// AddArguments function takes variadic amount of args.
+// Appends them to type Commands which underlying type is
+// []*Arg
 func AddArguments(args ...*Arg) Commands {
 	cmd := Commands{}
 	cmd = append(cmd, args...)
