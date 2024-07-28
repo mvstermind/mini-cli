b5c713e645829055ef12fe7ef4aecdac167b8f72
mvstermind <zablockidawid1@gmail.com>
2024-07-28 21:25:56 +0200

    docs: add more details into example + add found bug

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Minimalistic library for creating cli apps. Written in Go
 - convert values form string to default types if they are not strings []
 - make it work without not required args [x]
 ... if i'll come up with something more i'll update it
+
+
+# found bugs:
+- when calling -h program won't work beacuse of not passing required args
+
--- examples/simple-example.go
+++ examples/simple-example.go
@@ -3,24 +3,38 @@ package main
 import (
 	"fmt"
 
-	minicli "github.com/mvstermind/mini-cli"
+	mini "github.com/mvstermind/mini-cli"
 )
 
 // example
 func main() {
-	del := minicli.NewArg("d", "delete", "Deletes stuff", true, "rm")
-	undo := minicli.NewArg("u", "undo", "Undo stuff", true, 2)
-	revert := minicli.NewArg("r", "revert", "Reverts stuff", false, "-2")
 
-	cmds := minicli.AddArguments(del, undo, revert)
+	// argument can be created by filling a Arg struct or by using NewArg function
+	change := mini.Arg{
+		ShortCmd: "c",
+		LongCmd:  "change",
+		Usage:    "Example usage",
+		Required: true,
+		Default:  "change nothing",
+	}
 
+	del := mini.NewArg("d", "delete", "Deletes stuff", true, "rm")
+	undo := mini.NewArg("u", "undo", "Undo stuff", true, 2)
+	revert := mini.NewArg("r", "revert", "Reverts stuff", false, "-2")
+
+	// if arugment was created using struct, it must be passed to AddArguments using "&"
+	cmds := mini.AddArguments(&change, del, undo, revert)
+
+	// return values of arguments that were found in os.Args
 	argValues := cmds.Execute()
 
-	// To acces value of flag, index into map argValues with shorter version of flag name
+	// To acces value of flag, index into map argValues with short version of flag name
+	// to acces value of "revert" value use argValues["-r"]
 	for v := range argValues {
 		fmt.Println(argValues[v])
 	}
 
+	//note self:
 	// noticed that os.Args are str by default, find a way to make convert them afterwards
 	// fmt.Printf("%t\n", argValues["-u"])
 }
